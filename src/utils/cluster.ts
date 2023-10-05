3ef5e16f7805327951e9c087b9b31b1b22dfab5b
Kristofer Peterson <svenski123@users.noreply.github.com>
2023-10-05 02:46:38 +0200

    Add localnet support. Note the https endpoint for localnet is actually just plain http.

--- src/utils/cluster.ts
+++ src/utils/cluster.ts
@@ -1,17 +1,19 @@
 const endpoint = {
   http: {
+    localnet: 'http://127.0.0.1:8899',
     devnet: 'http://api.devnet.solana.com',
     testnet: 'http://api.testnet.solana.com',
     'mainnet-beta': 'http://api.mainnet-beta.solana.com/',
   },
   https: {
+    localnet: 'http://127.0.0.1:8899',
     devnet: 'https://api.devnet.solana.com',
     testnet: 'https://api.testnet.solana.com',
     'mainnet-beta': 'https://api.mainnet-beta.solana.com/',
   },
 };
 
-export type Cluster = 'devnet' | 'testnet' | 'mainnet-beta';
+export type Cluster = 'devnet' | 'testnet' | 'mainnet-beta' | 'localnet';
 
 /**
  * Retrieves the RPC API URL for the specified cluster
